829e93f79ba03066a23dcab2d807f7cb6a6edec5
Shaun Wild <shaun.wild@sainsburys.co.uk>
2018-02-28 19:51:21 +0000

    hotfix logging

--- app/deeplogger.js
+++ app/deeplogger.js
@@ -4,21 +4,21 @@ const winston_options = {
     json: true,
     timestamp: true,
     stringify: (obj) => JSON.stringify(obj),
-    filename: 'deepmod.log'
+    filename: 'deepmod.log',
+    level: getLoggingLevel()
 }
 
 const logger = new (winston.Logger)({
     transports: [
-        new winston.transports.Console(Object.assign(winston_options, {level: 'debug'})),
-        new winston.transports.File(Object.assign(winston_options, {level: 'info'}))
+        new winston.transports.Console(winston_options),
+        new winston.transports.File(winston_options)
     ]
 })
 
-winston.level = getLoggingLevel()
+logger.info(`logging at level ${winston_options.level}`)
 
 function getLoggingLevel(){
     const level = (process.env.DEEP_LOGGING ? process.env.DEEP_LOGGING : 'info')
-    logger.info(`logging at level ${level}`)
 
     return level
 }
